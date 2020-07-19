04bb67cebcf0a4874c8d62f604b7ac70a984dd6c
DmitryDemid <dimon.demid15@gmail.com>
2020-07-18 23:46:56 -0400

    Creating container component for Users.tsx

---
+++ src/Components/Users/UsersContainer.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {connect} from "react-redux";
+import Users from "./Users";
+import {AppStateType} from "../../redux/reduxStore";
+import {followAC, setUsersAC, unFollowAC, UsersType} from "../../redux/usersReducer";
+
+type UsersMapStateToPropsType = {
+    users: Array<UsersType>
+}
+
+type UsersMapDispatchToPropsType = {
+    follow: (userID: number) => void
+    unFollow: (userID: number) => void
+    setUsers: (users: Array<UsersType>) => void
+}
+
+const MapStateToProps = (state: AppStateType): UsersMapStateToPropsType => {
+    return {
+        users: state.usersPage.users
+    };
+};
+
+const MapDispatchToProps = (dispatch: any): UsersMapDispatchToPropsType => {
+    return {
+        follow: (userID: number) => {
+            dispatch(followAC(userID));
+        },
+        unFollow: (userID: number) => {
+            dispatch(unFollowAC(userID));
+        },
+        setUsers: (users: Array<UsersType>) => {
+            dispatch(setUsersAC(users));
+        },
+
+    };
+};
+
+
+const UsersContainer = connect(MapStateToProps, MapDispatchToProps)(Users);
+
+export default UsersContainer;
