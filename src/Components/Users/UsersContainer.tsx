4a32da20712fe79ef7361193ef4fe771e3267685
DmitryDemid <dimon.demid15@gmail.com>
2020-07-30 23:05:34 -0400

    Combine UserAPI with UsersContainer

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -10,11 +10,8 @@ import {
     UsersStateType,
     UsersType
 } from "../../redux/UsersReducer/usersReducer";
-import UsersAPI from "./UsersAPI";
-
-// type UsersMapStateToPropsType = {
-//     users: Array<UsersType>
-// }
+import axios from "axios";
+import UsersFunc from "./UsersFunc";
 
 type UsersMapDispatchToPropsType = {
     follow: (userID: number) => void
@@ -22,6 +19,44 @@ type UsersMapDispatchToPropsType = {
     setUsers: (users: Array<UsersType>) => void
     setCurrentPage: (currentPage: number) => void
     setTotalUsersCount: (totalCount: number) => void
+}
+
+type UsersAPIPropsType = UsersMapDispatchToPropsType &
+    {
+        users: Array<UsersType>
+        pageSize: number
+        totalUsersCount: number
+        currentPage: number
+    }
+
+class UsersAPI extends React.Component<UsersAPIPropsType> {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response: any) => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChange = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then((response: any) => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <UsersFunc
+            users={this.props.users}
+            currentPage={this.props.currentPage}
+            pageSize={this.props.pageSize}
+            totalUsersCount={this.props.totalUsersCount}
+            follow={this.props.follow}
+            unFollow={this.props.unFollow}
+            onPageChange={this.onPageChange}
+        />
+    }
 }
 
 const MapStateToProps = (state: AppStateType): UsersStateType => {
@@ -52,7 +87,6 @@ const MapDispatchToProps = (dispatch: any): UsersMapDispatchToPropsType => {
         },
     };
 };
-
 
 const UsersContainer = connect(MapStateToProps, MapDispatchToProps)(UsersAPI);
 
