84a5b7e7fe91da994f540a7b1425f8305b1cf861
DmitryDemid <dimon.demid15@gmail.com>
2020-08-06 21:09:35 -0400

    Adding new feature authentication in Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+type HeaderType = {
+    isAuth: boolean
+    login: string
+}
+
+const Header = (props: HeaderType) => {
     return (
         <header className={s.header}>
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Approve_icon.svg/1024px-Approve_icon.svg.png"
-                alt=""/>
+                alt="logo"/>
+                <div className={s.loginBlock}>
+                    {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                </div>
         </header>
     )
 }
