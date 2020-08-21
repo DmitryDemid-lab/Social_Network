550be8daa7e5b79c1ff54309019e87ebd737f6a2
DmitryDemid <dimon.demid15@gmail.com>
2020-08-21 18:00:59 -0400

    Creating login page

---
+++ src/Components/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Login.module.css';
+
+type LoginTypes = {}
+
+const Login = (props: LoginTypes) => {
+    return <div className={s.Login}>
+        <h1>Login</h1>
+    </div>
+}
+
+export default Login;
