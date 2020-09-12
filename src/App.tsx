6fad96f41c01d603c923456fbd048df63d2a7b63
DmitryDemid <dimon.demid15@gmail.com>
2020-09-11 22:02:30 -0400

    Connecting to state and adding new feature to initialize app

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,61 @@ import {Route} from "react-router-dom"
 import News from "./Components/News/News";
 import Music from "./Components/Music/Music";
 import Settings from "./Components/Settings/Settings";
-import store from "./redux/reduxStore"
+import store, {AppStateType} from "./redux/reduxStore"
 import UsersContainer from "./Components/Users/UsersContainer";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderConnect from "./Components/Header/Header.container";
 import Login from "./Components/Login/Login";
 import Dialogs from "./Components/Dialogs/DialogsContainer";
+import {connect} from "react-redux";
+import {initializeApp} from "./redux/appReducer/appReducer";
+import Preloader from "./Components/common/preloader/Preloader";
 
-function App() {
-    let state = store.getState();
-    return (
-        <div className="app-wrapper">
-            <HeaderConnect/>
-            <NavBar friends={state.sideBar.friends}/>
-            <div className="app-wrapper-content">
-                <Route path="/profile/:userId?"
-                       render={() => <ProfileContainer/>}/>
-                <Route path="/dialogs"
-                       render={() => <Dialogs/>}/>
-                <Route path="/news"
-                       render={() => <News/>}/>
-                <Route path="/music"
-                       render={() => <Music/>}/>
-                <Route path="/settings"
-                       render={() => <Settings/>}/>
-                <Route path="/users"
-                       render={() => <UsersContainer/>}/>
-                <Route path="/login"
-                       render={() => <Login/>}/>
+
+class App extends React.Component<AppPropsType> {
+    componentDidMount() {
+        this.props.initializeApp()
+    }
+
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
+            <div className="app-wrapper">
+                <HeaderConnect/>
+                <NavBar friends={store.getState().sideBar.friends}/>
+                <div className="app-wrapper-content">
+                    <Route path="/profile/:userId?"
+                           render={() => <ProfileContainer/>}/>
+                    <Route path="/dialogs"
+                           render={() => <Dialogs/>}/>
+                    <Route path="/news"
+                           render={() => <News/>}/>
+                    <Route path="/music"
+                           render={() => <Music/>}/>
+                    <Route path="/settings"
+                           render={() => <Settings/>}/>
+                    <Route path="/users"
+                           render={() => <UsersContainer/>}/>
+                    <Route path="/login"
+                           render={() => <Login/>}/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
+    initialized: state.app.initialized
+})
+
+export default connect(mapStateToProps, {initializeApp})(App);
+
+//TYPES
+type MapStateToPropsType = {
+    initialized: boolean
+}
+type AppPropsType = MapStateToPropsType & {
+    initializeApp: () => void
+}
