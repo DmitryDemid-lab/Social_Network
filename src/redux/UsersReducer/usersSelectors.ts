58dd856d446137e857d3044407d4ee85d703d08c
DmitryDemid <dimon.demid15@gmail.com>
2020-09-12 14:31:29 -0400

    adding createSelector example

--- src/redux/UsersReducer/usersSelectors.ts
+++ src/redux/UsersReducer/usersSelectors.ts
@@ -6,3 +6,5 @@ export const getTotalUsersCount = (state: AppStateType) => state.usersPage.total
 export const getCurrentPage = (state: AppStateType) => state.usersPage.currentPage
 export const getIsFetching = (state: AppStateType) => state.usersPage.isFetching
 export const getFollowingInProgress = (state: AppStateType) => state.usersPage.followingInProgress
+/*//Reselect example
+export const getUsersSuperSelector = createSelector(getUsersSelector, (users: Array<UsersType>) => users.filter(u => true))*/
