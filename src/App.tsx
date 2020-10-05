a63728a29cadfe66b81332a5aa0282fe13151b31
DmitryDemid <dimon.demid15@gmail.com>
2020-10-04 23:31:32 -0400

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import NavBar from "./Components/NavBar/Navbar";
-import {HashRouter, Route} from "react-router-dom"
+import {HashRouter, Redirect, Route, Switch} from "react-router-dom"
 import News from "./Components/News/News";
 import Music from "./Components/Music/Music";
 import Settings from "./Components/Settings/Settings";
@@ -10,7 +10,7 @@ import UsersContainer from "./Components/Users/UsersContainer";
 import HeaderConnect from "./Components/Header/Header.container";
 import Login from "./Components/Login/Login";
 import {connect, Provider} from "react-redux";
-import {initializeApp} from "./redux/appReducer/appReducer";
+import {handleError, initializeApp} from "./redux/appReducer/appReducer";
 import Preloader from "./Components/common/preloader/Preloader";
 import {WithSuspense} from "./hoc/WithSuspense";
 
@@ -19,8 +19,19 @@ const ProfileContainer = React.lazy(() => import('./Components/Profile/ProfileCo
 
 
 class App extends React.Component<AppPropsType> {
+
+    catchAllUnhandledRejections = (promiseRejectionEvent: any) => {
+        this.props.handleError(promiseRejectionEvent.reason.message)
+        console.log(promiseRejectionEvent)
+    }
+
     componentDidMount() {
         this.props.initializeApp()
+        window.addEventListener('unhandledrejection', this.catchAllUnhandledRejections)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('unhandledrejection', this.catchAllUnhandledRejections)
     }
 
     render() {
@@ -29,43 +40,42 @@ class App extends React.Component<AppPropsType> {
         }
         return (
             <div className="app-wrapper">
-
                 <HeaderConnect/>
                 <NavBar friends={store.getState().sideBar.friends}/>
                 <div className="app-wrapper-content">
-                    <Route path="/profile/:userId?"
-                           render={WithSuspense(ProfileContainer)}/>
-                    <Route path="/dialogs"
-                           render={WithSuspense(Dialogs)}/>
-                    <Route path="/news"
-                           render={() => <News/>}/>
-                    <Route path="/music"
-                           render={() => <Music/>}/>
-                    <Route path="/settings"
-                           render={() => <Settings/>}/>
-                    <Route path="/users"
-                           render={() => <UsersContainer/>}/>
-                    <Route path="/login"
-                           render={() => <Login/>}/>
+                    {/*ВСТАВИТЬ КНОПКУ С ЗАНУЛЕНИЕМ ОШИБКИ*/}
+                    {this.props.error && <div className='error'><h4>{this.props.error}</h4></div>}
+                    <Switch>
+                        <Route path='/' exact><Redirect to='/profile'/></Route>
+                        <Route path="/profile/:userId?"
+                               render={WithSuspense(ProfileContainer)}/>
+                        <Route path="/dialogs"
+                               render={WithSuspense(Dialogs)}/>
+                        <Route path="/news"
+                               render={() => <News/>}/>
+                        <Route path="/music"
+                               render={() => <Music/>}/>
+                        <Route path="/settings"
+                               render={() => <Settings/>}/>
+                        <Route path="/users"
+                               render={() => <UsersContainer/>}/>
+                        <Route path="/login"
+                               render={() => <Login/>}/>
+                        <Route path="*" exact
+                               render={() => <div><h2>404 NOT FOUND</h2></div>}/>
+                    </Switch>
                 </div>
             </div>
         )
     }
 }
 
-/*
-<React.Suspense fallback={<Spinner />}>
-      <div>
-        <OtherComponent />
-      </div>
-    </React.Suspense>
-*/
-
 const mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
-    initialized: state.app.initialized
+    initialized: state.app.initialized,
+    error: state.app.error,
 })
 
-const AppContainer = connect(mapStateToProps, {initializeApp})(App);
+const AppContainer = connect(mapStateToProps, {initializeApp, handleError})(App);
 
 export const MainApp = () => {
     return <HashRouter>
@@ -78,7 +88,9 @@ export const MainApp = () => {
 //TYPES
 type MapStateToPropsType = {
     initialized: boolean
+    error: string | null
 }
 type AppPropsType = MapStateToPropsType & {
     initializeApp: () => void
+    handleError: (error: string | null) => void
 }
--- src/redux/appReducer/appReducer.ts
+++ src/redux/appReducer/appReducer.ts
@@ -1,9 +1,11 @@
 import {getAuthUserData} from "../AuthReducer/AuthReducer";
 
 const SET_INITIALIZED_SUCCESS = 'app/SET_INITIALIZED_SUCCESS';
+const SET_ERROR = 'app/SET_ERROR';
 
 let initialState: AuthInitialStateType = {
-    initialized: false
+    initialized: false,
+    error: null
 };
 
 export const appReducer = (state: AuthInitialStateType = initialState, action: AppActionsType) => {
@@ -13,6 +15,11 @@ export const appReducer = (state: AuthInitialStateType = initialState, action: A
                 ...state,
                 initialized: true
             };
+        case SET_ERROR:
+            return {
+                ...state,
+                error: action.error
+            }
         default:
             return state;
     }
@@ -20,6 +27,7 @@ export const appReducer = (state: AuthInitialStateType = initialState, action: A
 
 //ACTIONS
 export const setInitializedSuccess = () => ({type: SET_INITIALIZED_SUCCESS} as const)
+export const setError = (error: string | null) => ({type: SET_ERROR, error} as const)
 
 //THUNKS
 export const initializeApp = () => (dispatch: any) =>
@@ -27,9 +35,15 @@ export const initializeApp = () => (dispatch: any) =>
         .then(() => {
             dispatch(setInitializedSuccess())
         })
-
+export const handleError = (error: string | null) => (dispatch: any) => {
+    dispatch(setError(error))
+}
 //TYPES
-export type AppActionsType = ReturnType<typeof setInitializedSuccess>
+export type AppActionsType =
+    | ReturnType<typeof setInitializedSuccess>
+    | ReturnType<typeof setError>
+
 export type AuthInitialStateType = {
     initialized: boolean
+    error: string | null
 }
